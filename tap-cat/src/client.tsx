67705dbc0ae2afacb9f000e08edb1d706a4029cb
Katrina W. <katrina.elise.wright@gmail.com>
2024-05-16 23:47:40 -0500

    proof of concept!

--- tap-cat/src/client.tsx
+++ tap-cat/src/client.tsx
@@ -6,6 +6,20 @@ import "./styles.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <img src="src/assets/Cute Cat Eating Fish.gif" useMap="#image-map"></img>
+
+<map name="image-map">
+    <area target="_blank" id = "Face" alt="Face" title="Face" onClick={() => console.log("BOOM! Face!")} coords="594,446,255" shape="circle"></area>
+    <area target="_blank" alt="backFootLeft" title="backFootLeft" onClick={() => console.log("BOOM! foot 2!")} coords="785,933,932,1020" shape="rect"></area>
+    <area target="_blank" alt="backFootRight" title="backFootRight" onClick={() => console.log("BOOM! Foot 1!")} coords="261,933,397,1018" shape="rect"></area>
+    <area target="_blank" alt="foodBowl" title="foodBowl" onClick={() => console.log("BOOM! Food!")} coords="416,1079,442,981,453,952,472,942,495,934,505,929,513,905,540,896,566,899,591,908,602,922,632,923,665,930,694,935,723,947,736,961,747,975,753,988,756,1008,760,1029,764,1046,770,1069,772,1080,752,1094,672,1122,566,1127,450,1106,423,1093" shape="poly"></area>
+    <area target="_blank" alt="mainBody" title="mainBody" onClick={() => console.log("BOOM! Body!")} coords="429,1017,405,1005,402,932,305,922,314,839,348,782,380,757,387,688,405,646,501,685,570,706,644,703,701,684,748,663,786,657,802,723,803,764,847,788,873,831,881,884,882,916,880,924,792,924,785,940,782,1002,763,1013,755,970,727,943,696,933,660,923,624,919,604,916,620,899,660,877,681,828,677,786,651,754,614,740,568,737,530,762,507,807,512,839,538,884,540,890,518,894,502,907,498,924,469,936,446,949" shape="poly"></area>
+    <area target="_blank" alt="sensitiveBelly" title="sensitiveBelly" onClick={() => console.log("BOOM! Belly Time!")} coords="595,822,81" shape="circle"></area>
+    <area target="_blank" alt="tailUp" title="tailUp" onClick={() => console.log("BOOM! Tail Up!")} coords="882,921,959,638" shape="rect"></area>
+    <area target="_blank" alt="tailDown" title="tailDown" onClick={() => console.log("BOOM! Tail Down!")} coords="963,849,1074,976" shape="rect"></area>
+    <area target="_blank" alt="outerHead" title="outerHead" onClick={() => console.log("BOOM! Ears, outer head!")} coords="402,640,372,705,273,642,252,567,236,523,267,378,217,214,233,133,448,55,567,164,663,173,756,81,808,69,1012,182,956,396,981,477,972,555,926,636,879,636,873,716,826,710,813,741,806,704,798,674,792,647,754,653,799,611,834,552,850,490,852,421,843,360,811,299,767,248,713,216,678,198,599,186,515,194,440,230,396,272,362,324,339,396,333,460,344,514,372,576,402,624" shape="poly"></area>
+    <area target="_blank" alt="tailMid" title="tailMid" onClick={() => console.log("BOOM! Tail Mid!")} coords="966,764,1029,843,1070,841,1045,757,968,705" shape="poly"></area>
+</map>
     <App />
   </React.StrictMode>
 )
