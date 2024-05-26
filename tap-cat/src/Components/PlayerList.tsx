2963463e5cf4c48543bc685d5d1e32b532974a2b
katrinaWright <katrina.elise.wright@gmail.com>
2024-05-26 13:50:54 -0500

    adding icons to player cards

--- tap-cat/src/Components/PlayerList.tsx
+++ tap-cat/src/Components/PlayerList.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { PlayerId } from "rune-games-sdk/multiplayer";
 import scratchIcon from '../assets/scratch-icon.png'; // Adjust the path as needed
+import pointsIcon from '../assets/heart-eyes-cat.svg'; // Adjust the path as needed
 import { GameState } from '../logic';
+import './PlayerList.css'; // Make sure to create and adjust the path as needed
 
 interface PlayerListProps {
   playerIds: PlayerId[];
@@ -12,30 +14,35 @@ interface PlayerListProps {
 
 const PlayerList: React.FC<PlayerListProps> = ({ playerIds, game, yourPlayerId, scratches }) => {
   return (
-    <ul id="playersSection">
+    <div className="player-list">
       {playerIds.map((playerId, index) => {
         const player = Rune.getPlayerInfo(playerId);
 
         return (
-          <li key={playerId} data-player={index.toString()}>
-            <img src={player.avatarUrl} alt={`${player.displayName}'s avatar`} />
-            <span>
-              <div>{game.scores[playerId]}</div>
-              <div>{player.displayName}</div>
-              <div>
-                {scratches[playerId]} <img src={scratchIcon} alt="scratch icon" style={{ width: '24px', height: '24px' }} />
+          <div key={playerId} className="player-card" data-player={index.toString()}>
+            <div className="player-info">
+              <img className="avatar" src={player.avatarUrl} alt={`${player.displayName}'s avatar`} />
+              <span className="player-name">
+                {player.displayName}
+                {player.playerId === yourPlayerId && <span> (You)</span>}
+              </span>
+            </div>
+            <div className="player-stats">
+              <div className="stat">
+              {game.scores[playerId]}
+                <img src={pointsIcon} alt="points icon" className="icon" />
+                
               </div>
-              {player.playerId === yourPlayerId && (
-                <span>
-                  <br />
-                  (You)
-                </span>
-              )}
-            </span>
-          </li>
+              <div className="stat">
+              {scratches[playerId]}
+                <img src={scratchIcon} alt="scratch icon" className="icon" />
+                
+              </div>
+            </div>
+          </div>
         );
       })}
-    </ul>
+    </div>
   );
 };
 
