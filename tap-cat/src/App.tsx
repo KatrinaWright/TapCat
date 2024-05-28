52863f53ecb0b69b72a657c2e76f4674356c3fd8
D-Fluster <158021342+D-Fluster@users.noreply.github.com>
2024-05-27 22:12:38 -0700

    Adding notes to self (DKF)

--- tap-cat/src/App.tsx
+++ tap-cat/src/App.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState, useRef  } from "react";
+import { useEffect, useState, useRef } from "react";
 import { PlayerId } from "rune-games-sdk/multiplayer";
-import catHappyPurr from "./assets/purring-cat-156459.mp3"
-import catMadSound from "./assets/sat-on-the-cat-95941.mp3"
+import catHappyPurr from "./assets/purring-cat-156459.mp3";
+import catMadSound from "./assets/sat-on-the-cat-95941.mp3";
 import { GameState } from "./logic";
 import PettingZones from "./Components/PettingZones";
 import PlayerList from "./Components/PlayerList";
 import CatHappinessBar from "./Components/CatHappinessBar";
 import IdleAnimationOverlay from "./Components/IdleAnimationOverlay";
 import picture from "../src/assets/Cat Saying Hello.gif";
-import mapData from './mapData.json';
+import mapData from "./mapData.json";
+// Would need to import this as well
+// Sends mapData into pettingZones & imageMap (imageMap is fine)
+// Trick is keeping track of which JSON file attached to which cat gif
 
 //const selectSound = new Audio(selectSoundAudio);
 const MadSound = new Audio(catMadSound);
@@ -32,9 +35,8 @@ function App() {
         setYourPlayerId(yourPlayerId);
 
         if (action && action.name === "updateScratch") MadSound.play();
-        if (action && action.name === "updateScore" && game.catHappiness > 75 ) purrSound.play();
-        
-        
+        if (action && action.name === "updateScore" && game.catHappiness > 75)
+          purrSound.play();
       },
     });
 
@@ -46,7 +48,7 @@ function App() {
     }, 1000);
 
     return () => clearInterval(interval);
-    }, []);
+  }, []);
 
   if (!game) {
     // Rune only shows your game after an onChange() so no need for loading screen
@@ -55,18 +57,25 @@ function App() {
 
   const { playerIds, scratches, catHappiness } = game;
 
-  return (   
+  return (
     <div onMouseMove={handleInteraction} onTouchMove={handleInteraction}>
       <CatHappinessBar catHappiness={catHappiness} />
       <img src={picture} useMap="#image-map" alt="Petting Zones Map" />
+      {/* {picture will need updating/re-mapping} */}
       {yourPlayerId && (
         <PettingZones
           imageName="image-map"
           mapData={mapData}
+          // {mapData} would also need to change
           playerId={yourPlayerId}
         />
       )}
-      <PlayerList playerIds={playerIds} game={game} yourPlayerId={yourPlayerId} scratches={scratches} />
+      <PlayerList
+        playerIds={playerIds}
+        game={game}
+        yourPlayerId={yourPlayerId}
+        scratches={scratches}
+      />
       {yourPlayerId && <IdleAnimationOverlay idle={idle} />}
     </div>
   );
